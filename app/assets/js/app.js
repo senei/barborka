46ce9afd95a7e98d71bc20772877da8b247b3733
senei <senei.web@gmail.com>
2014-06-08 13:56:53 +0200

    new oall

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,152 +1,160 @@
 $(function() {
-    var $magicLine = $("#menu #magic-line");
-    function init(){
-        if($magicLine.length == 1){
-        if($(".current_page a").length == 1){
-            $magicLine
-                .width($(".current_page a").width() + 4)
-                .css("left", $(".current_page a").position().left - 2)
-                .css("top", $(".current_page a").position().top + 26);
-        }
-        $magicLine
-            .data("origTop", Math.floor($magicLine.position().top))
-            .data("origLeft", Math.floor($magicLine.position().left))
-            .data("origWidth", $magicLine.width());
-        }
-    }
-    function active(){
-        $el = $('li.active').find('a');
-        
-        if($el.position()){
-            leftPos = Math.floor($el.position().left-2);
-            topPos = Math.floor($el.position().top+26);
-            newWidth = $el.width()+4;
-        } else {
-            leftPos = -2;
-            topPos = 28;
-            newWidth = 0;
-        }
+	var $magicLine = $("#menu #magic-line");
 
-        $magicLine.stop().animate({
-            left: leftPos,
-            top: topPos,
-            width: newWidth
-        });
-    }
-    init();
-    window.onresize = function(event) {
-        init();
-    };
-    $("#menu li").hover(function() {
-        $el = $(this).find('a');
-        
-            $magicLine
-                .width($(".active a").width() + 4)
-                .css("left", $(".active a").position().left - 2)
-                .css("top", $(".active a").position().top + 26);
-        if($el.position()){
-            leftPos = Math.floor($el.position().left-2);
-            topPos = Math.floor($el.position().top+26);
-            newWidth = $el.width()+4;
-        } else {
-            leftPos = -2;
-            topPos = 28;
-            newWidth = 0;
-        }
-        $magicLine.stop().animate({
-            left: leftPos,
-            top: topPos,
-            width: newWidth
-        });
-    }, function() {
-        $magicLine.stop().animate({
-            top: $magicLine.data("origTop"),
-            left: $magicLine.data("origLeft"),
-            width: $magicLine.data("origWidth")
-        });    
-    });
+	function init() {
+		if ($magicLine.length == 1) {
+			if ($(".current_page a").length == 1) {
+				$magicLine
+					.width($(".current_page a").width() + 4)
+					.css("left", $(".current_page a").position().left - 2)
+					.css("top", $(".current_page a").position().top + 26);
+			}
+			$magicLine
+				.data("origTop", Math.floor($magicLine.position().top))
+				.data("origLeft", Math.floor($magicLine.position().left))
+				.data("origWidth", $magicLine.width());
+		}
+	}
 
-// Cache selectors
-var lastId,
-    topMenu = $("header"),
-    topMenuHeight = topMenu.outerHeight(),
-    // All list items
-    menuItems = topMenu.find("a[href^='/#']"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-        var href = $(this).attr("href").slice( 1 );
-      var item = $(href);
-      if (item.length) { return item; }
-    });
+	function active() {
+		$el = $('li.active').find('a');
 
-    // Bind click handler to menu items
-    // so we can get a fancy scroll animation
-    menuItems.click(function(e){
-      var href = $(this).attr("href").slice( 1 ),
-          offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
-      $('html, body').stop().animate({ 
-          scrollTop: offsetTop
-      }, 300);
-      e.preventDefault();
-    });
+		if ($el.position()) {
+			leftPos = Math.floor($el.position().left - 2);
+			topPos = Math.floor($el.position().top + 26);
+			newWidth = $el.width() + 4;
+		} else {
+			leftPos = -2;
+			topPos = 28;
+			newWidth = 0;
+		}
 
-    // Bind to scroll
-    $(window).scroll(function(){
-       // Get container scroll position
-       var fromTop = $(this).scrollTop()+topMenuHeight;
-       
-       // Get id of current scroll item
-       var cur = scrollItems.map(function(){
-         if ($(this).offset().top < fromTop)
-           return this;
-       });
-       // Get the id of the current element
-       cur = cur[cur.length-1];
-       var id = cur && cur.length ? cur[0].id : "";
-       
-       if (lastId !== id) {
-           lastId = id;
-           // Set/remove active class
-           menuItems
-             .parent().removeClass("active")
-             .end().filter("[href='/#"+id+"']").parent().addClass("active");
-            active();
-       }                   
-    });
+		$magicLine.stop().animate({
+			left: leftPos,
+			top: topPos,
+			width: newWidth
+		});
+	}
+	init();
+	window.onresize = function(event) {
+		init();
+	};
+	$("#menu li").hover(function() {
+		$el = $(this).find('a');
+		if ($el.position()) {
+			leftPos = Math.floor($el.position().left - 2);
+			topPos = Math.floor($el.position().top + 26);
+			newWidth = $el.width() + 4;
+		} else {
+			leftPos = -2;
+			topPos = 28;
+			newWidth = 0;
+		}
+		$magicLine.stop().animate({
+			left: leftPos,
+			top: topPos,
+			width: newWidth
+		});
+	}, function() {
+		$magicLine.stop().animate({
+			top: $magicLine.data("origTop"),
+			left: $magicLine.data("origLeft"),
+			width: $magicLine.data("origWidth")
+		});
+	});
 
-    //games
+	// Cache selectors
+	var lastId,
+		topMenu = $("header"),
+		topMenuHeight = topMenu.outerHeight(),
+		// All list items
+		menuItems = topMenu.find("a[href^='/#']"),
+		// Anchors corresponding to menu items
+		scrollItems = menuItems.map(function() {
+			var href = $(this).attr("href").slice(1);
+			var item = $(href);
+			if (item.length) {
+				return item;
+			}
+		});
 
-    $('ul.tabs li').on('touchstart click', function(){
-        $('ul.tabs li').removeClass('selected');
-        $('ul.content li').removeClass('selected');
-        //
-        num = $('ul.tabs li').index(this);
-        $('ul.tabs li').eq(num).addClass('selected');
-        $('ul.content li').eq(num).addClass('selected');
-    });
-        
-    if ( $("#memoGame").length > 0 ){
-	    new memoGame('memoGame').init();
-	  }
-    if ( $("#slownikGameBord").length > 0 ){
-	    setTimeout(function () {
-			$("#slownikGameBord li").removeClass('off');
-			$("#slownikGameBord li button").on('touchstart click', function(){
-			var li = $(this).parent();
-        	if(li.hasClass('no-off')){
-				li.addClass('no').removeClass('no-off');
-				setTimeout(function () {
-					li.addClass('no-off').removeClass('no');
-      			}, 1000);
-        	} else if (li.hasClass('yes-off')) {
-				li.addClass('yes').removeClass('yes-off');
-      			setTimeout(function () {
-                    li.addClass('yes-off').removeClass('yes');
-                    $("#slownikGameBord li").addClass('off');
-                }, 2000);
-        	}
-        });
-      }, 1000);
-	    
-	  }
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+	menuItems.click(function(e) {
+		var href = $(this).attr("href").slice(1),
+			offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+		$('html, body').stop().animate({
+			scrollTop: offsetTop
+		}, 300);
+		e.preventDefault();
+	});
+
+	// Bind to scroll
+	$(window).scroll(function() {
+		// Get container scroll position
+		var fromTop = $(this).scrollTop() + topMenuHeight;
+
+		// Get id of current scroll item
+		var cur = scrollItems.map(function() {
+			if ($(this).offset().top < fromTop)
+				return this;
+		});
+		// Get the id of the current element
+		cur = cur[cur.length - 1];
+		var id = cur && cur.length ? cur[0].id : "";
+
+		if (lastId !== id) {
+			lastId = id;
+			// Set/remove active class
+			menuItems
+				.parent().removeClass("active")
+				.end().filter("[href='/#" + id + "']").parent().addClass("active");
+			active();
+		}
+	});
+
+	//games
+
+	$('ul.tabs li').on('touchstart click', function() {
+		$('ul.tabs li').removeClass('selected');
+		$('ul.content li').removeClass('selected');
+		//
+		num = $('ul.tabs li').index(this);
+		$('ul.tabs li').eq(num).addClass('selected');
+		$('ul.content li').eq(num).addClass('selected');
+	});
+
+	if ($("section.games").length > 0) {
+		height = Math.floor($(window).height() / 3 - 180 / 3);
+		width = 40 + (height) * 4 + "px";
+		height = 40 + (height) * 3 + "px";
+		$("section.games").css('max-width', width).css('max-height', height);
+
+		if ($("#memoGame").length > 0) {
+			new memoGame('memoGame').init();
+		}
+		if ($("#slownikGameBord").length > 0) {
+			new slowaGame('memoGame').init();	
+
+			// setTimeout(function() {
+			// 	$("#slownikGameBord li").removeClass('off');
+			// 	$("#slownikGameBord li button").on('touchstart click', function() {
+			// 		var li = $(this).parent();
+			// 		if (li.hasClass('no-off')) {
+			// 			li.addClass('no').removeClass('no-off');
+			// 			setTimeout(function() {
+			// 				li.addClass('no-off').removeClass('no');
+			// 			}, 1000);
+			// 		} else if (li.hasClass('yes-off')) {
+			// 			li.addClass('yes').removeClass('yes-off');
+			// 			setTimeout(function() {
+			// 				li.addClass('yes-off').removeClass('yes');
+			// 				$("#slownikGameBord li").addClass('off');
+			// 			}, 2000);
+			// 		}
+			// 	});
+			// }, 1000);
+
+		}
+	}
 });
--- app/assets/js/memo.js
+++ app/assets/js/memo.js
@@ -10,7 +10,7 @@
             boardWidth: 4,
             boardHeight: 2,
             imageCount: 4,
-            imageMaxCount: 4,
+            imageMaxCount: 29,
             selected: 0,
             firstClick: false,
 
@@ -22,8 +22,14 @@
             secondIndexY: null,
 
             tab: [],
-            imgTab: [1,2,3,4,5,6],
-            level: 0
+            imgTab: [
+                0,1,2,3,4,5,
+                6,7,8,9,10,11,
+                12,13,14,15,16,17,
+                18,19,20,21,22,23,
+                24,25,26,27,28 ],
+            level: 0,
+            size: 0
 
         };
 
@@ -32,14 +38,14 @@
             self.data.level ++;
 
             self.dom.images = jQuery('#imagesCache');
-            if(self.dom.images.find('li').length == 0){
-                self.dom.images.hide();
-                self.dom.images.append('<li></li>');
-                for (var i = 1; i <= self.data.imageMaxCount; i++) {
-                    self.dom.images.append('<li><img src="/assets/img/gry/thiles/boardThile_' + i + '.png" alt="memo' + i + '" /></li>');
+            // if(self.dom.images.find('li').length == 0){
+            //     self.dom.images.hide();
+            //     self.dom.images.append('<li></li>');
+            //     for (var i = 1; i <= self.data.imageMaxCount; i++) {
+            //         self.dom.images.append('<li><img src="/assets/img/gry/thiles/karta_' + i + '.png" alt="memo_' + i + '" /></li>');
                     
-                }
-            }
+            //     }
+            // }
 
             self.checkLevel();
             
@@ -49,28 +55,39 @@
                 tabNumbers.push(i);
             }
             
-            var iteration = 7 - self.data.imageCount;
+            iteration = self.data.imageCount;
+            while (--iteration) self.randomizeArray(tabNumbers);
+
+            var iteration = 29 - self.data.imageCount;
             while (--iteration) {
                 var imgTabIndex = Math.floor( Math.random() * self.data.imgTab.length );
                 self.data.imgTab.splice(imgTabIndex, 1);
             }
-
-            iteration = self.data.imageCount;
-            while (--iteration) self.randomizeArray(tabNumbers);
-
-            // if(self.data.boardWidth == 3) self.dom.board.css("width","550px"); 
-            // else self.dom.board.css("width","720px"); 
 
             var counter = 0;
             for (var i = 0; i < self.data.boardHeight; i++) {
                 self.data.tab[i] = [];
                 for (var j = 0; j < self.data.boardWidth; j++) {
                     self.data.tab[i][j] = tabNumbers[counter++];
-                    self.dom.board.append("<li id='board_"+i+"_"+j+"' />");
-                    height = $('#board_0_0').width();
-                    $('#board_'+ i + '_'+j).height(height);
+                    self.dom.board.append("<li id='board_"+i+"_"+j+"' class='memo_"+(counter%3)+"'/>");
+                    
+                    if(self.data.size == 0){
+                        self.data.size = $('#board_0_0').width();
+                        height = $(window).height()-180;
+                        console.log(height);
+                        if(height / 3 < self.data.size) self.data.size = height / 3;
+                        width = 40+(self.data.size)*4+"px";
+                        self.dom.board.css('max-width',width);
+                    }
+                    $('#board_'+ i + '_'+j).height(self.data.size).width(self.data.size);
                 }
             }
+            var gornik = self.dom.images.find('li').eq(0).find("img").clone();
+            self.dom.board.append(gornik);
+            
+            // if(self.data.boardWidth == 3) self.dom.board.css("margin-top", "0px");
+            // else self.dom.board.css("margin-top", (self.data.size/2) + "px");
+
             self.dom.board.find('li').on('touchstart click', function () {
                 self.checkClick($(this));
             });
@@ -78,10 +95,15 @@
         };
 
         window.onresize = function(event) {
-            self.init();
+            location.reload();
         };
         self.checkLevel = function () {
-            self.data.imgTab = [1,2,3,4,5,6];
+            self.data.imgTab = [
+                0,1,2,3,4,5,
+                6,7,8,9,10,11,
+                12,13,14,15,16,17,
+                18,19,20,21,22,23,
+                24,25,26,27,28 ];
             self.dom.board.find('li').remove();
             self.data.selected = 0;
             //
@@ -95,10 +117,9 @@
         self.checkClick = function (object) {
             var tabObject = (object.attr('id')).split('_');
             var number = self.data.tab[tabObject[1]][tabObject[2]];
-            var img = self.dom.images.find('li').eq(number).find("img").clone();
-            console.log(self.data.tab);
-            console.log(number);
+            var img = self.dom.images.find('li').eq(self.data.imgTab[number]).find("img").clone();
             self.dom.board.find('#' + object.attr('id')).html(img);
+
             if (!self.data.firstClick) {
                 self.data.firstClick = true;
                 self.data.firstNumber = number;
@@ -113,14 +134,17 @@
                         self.data.selected++;
                         self.dom.board.find('#board_' + self.data.firstIndexX + '_' + self.data.firstIndexY).unbind('click').css('cursor', 'default').addClass('blocked');
                         self.dom.board.find('#board_' + self.data.secondIndexX + '_' + self.data.secondIndexY).unbind('click').css('cursor', 'default').addClass('blocked');
-                        
-                        
-
+                       
                         if (self.data.selected == self.data.imageCount) {
-                            alert('Wygrałeś! Gratulacje! lev:'+self.data.level);
+                           $('div.gornik')
+                                    .css('background-position-x', "0%")
+                                    .css('bottom',20); 
                             setTimeout(function () {
-                                self.init();
-                            }, 1000);
+                                $('div.gornik')
+                                    .css('background-position-x', "100%")
+                                    .css('bottom',-400); 
+                                 self.init();
+                            }, 2000);
                         }
                     } else { // incorrect
                         self.dom.board.find('li').unbind('click');
