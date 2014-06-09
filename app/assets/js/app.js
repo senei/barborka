830e23e0bc950130cd355a3305dc5fe2db422125
senei <senei.web@gmail.com>
2014-06-09 23:53:53 +0200

    100% all

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -102,7 +102,7 @@ $(function() {
 		// Get the id of the current element
 		cur = cur[cur.length - 1];
 		var id = cur && cur.length ? cur[0].id : "";
-
+		
 		if (lastId !== id) {
 			lastId = id;
 			// Set/remove active class
