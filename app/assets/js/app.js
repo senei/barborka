4beffee7e39ef6bea13ee2fc22e5edf1bb049427
senei <senei.web@gmail.com>
2014-06-12 19:30:10 +0200

    100% all

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -101,41 +101,41 @@ $(function() {
 
 	// Bind click handler to menu items
 	// so we can get a fancy scroll animation
-	// menuItems.click(function(e) {
-	// 	var href = $(this).attr("href").slice(1),
-	// 		offsetTop = href === "/#" ? 0 : $(href).offset().top - topMenuHeight + 1;
-	// 	$('html, body').stop().animate({
-	// 		scrollTop: offsetTop
-	// 	}, 300);
-	// 	e.preventDefault();
-	// });
+	menuItems.click(function(e) {
+		var href = $(this).attr("href").slice(1),
+			offsetTop = href === "/#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+		$('html, body').stop().animate({
+			scrollTop: offsetTop
+		}, 300);
+		e.preventDefault();
+	});
 
 	// Bind to scroll
-	// $(window).scroll(function() {
-	// 	if ($(".current_page").length == 0) {
+	$(window).scroll(function() {
+		if ($(".current_page").length == 0) {
 
-	// 		// Get container scroll position
-	// 		var fromTop = $(this).scrollTop() + topMenuHeight;
+			// Get container scroll position
+			var fromTop = $(this).scrollTop() + topMenuHeight;
 
-	// 		// Get id of current scroll item
-	// 		var cur = scrollItems.map(function() {
-	// 			if ($(this).offset().top < fromTop)
-	// 				return this;
-	// 		});
-	// 		// Get the id of the current element
-	// 		cur = cur[cur.length - 1];
-	// 		var id = cur && cur.length ? cur[0].id : "";
+			// Get id of current scroll item
+			var cur = scrollItems.map(function() {
+				if ($(this).offset().top < fromTop)
+					return this;
+			});
+			// Get the id of the current element
+			cur = cur[cur.length - 1];
+			var id = cur && cur.length ? cur[0].id : "";
 			
-	// 		if (lastId !== id) {
-	// 			lastId = id;
-	// 			// Set/remove active class
-	// 			menuItems
-	// 				.parent().removeClass("active")
-	// 				.end().filter("[href*='/#" + id + "']").parent().addClass("active");
-	// 			active();
-	// 		}
-	// 	}
-	// });
+			if (lastId !== id) {
+				lastId = id;
+				// Set/remove active class
+				menuItems
+					.parent().removeClass("active")
+					.end().filter("[href*='/#" + id + "']").parent().addClass("active");
+				active();
+			}
+		}
+	});
 
 
 });
